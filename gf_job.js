0d1bf2fa9e86da9c9806aca8ed5a38c2c247375a
munezo <github@velvet.sakura.ne.jp>
2014-12-29 08:43:14 +0900

    add continuation-login-reward-animation

--- gf_job.js
+++ gf_job.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         ガールフレンド(仮) [バイト]
 // @namespace    https://twitter.com/stylish_munezo
-// @version      0.2
+// @version      0.3
 // @description  バイト自動継続
 // @author       Munezo
 // @match        *://vcard.ameba.jp/mypage
 // @match        *://vcard.ameba.jp/mypage/login-bonus*
 // @match        *://vcard.ameba.jp/mypage/cupid-login-animation*
 // @match        *://vcard.ameba.jp/mypage/date-bonus*
+// @match        *://vcard.ameba.jp/mypage/continuation-login-reward-animation*
 // @match        *://vcard.ameba.jp/job*
 // @match        *://vcard.ameba.jp/limited-login*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
